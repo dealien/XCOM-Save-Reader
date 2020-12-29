4e6caa3a6eb602f9c1d0e4bbfe355d7c3dc47555
Vyren Gray <dragonofawsome@gmail.com>
2020-12-29 16:39:35 -0500

    Basic UI

--- Copy Current Saves.bat
+++ Copy Current Saves.bat
@@ -1,3 +1,10 @@
-rem Copies current XCom saves to the local user folder. Purely for testing purposes.
-ROBOCOPY C:\Users\drago\Desktop\Gaming\OpenXcom\XPiratez\user\piratez user\piratez /MIR /XF README.txt
+rem Copies current XCom saves to a local user folder. Purely for testing purposes.
+if [%~1]==[] goto :noargs
+if [%~2]==[] goto :noargs
+ROBOCOPY "%~1" "%~2" /MIR /XF README.txt
+EXIT 0
+
+:noargs
+echo Expected two paths as arguments; switching to default paths.
+ROBOCOPY "%HomePath%\Desktop\Gaming\OpenXcom\XPiratez\user\piratez" "user\piratez" /MIR /XF README.txt
 EXIT 0
--- reader.py
+++ reader.py
@@ -1,12 +1,12 @@
 import csv
 import json
+import logging
 import os
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import *
 from tkinter import filedialog
 from tkintertable import TableCanvas, TableModel
-
 import yaml
 
 if os.name == 'posix':
@@ -18,16 +18,20 @@ else:
 
 USERDIR = os.path.join(ROOTDIR, 'user')
 
-debugmode = True
-jsondump = False
+# TODO: debug_mode and json_dump should become arguments
+debug_mode = True
+json_dump = True
 
-if debugmode is True:
-    file_path = os.path.join(ROOTDIR, 'user', 'piratez', '_quick_.asav')
+if debug_mode is True:
+    save_file_path = os.path.join(ROOTDIR, 'user', 'piratez', '_quick_.asav')
+    l = logging.DEBUG
 else:
-    root = tk.Tk()
-    root.withdraw()
+    save_file_path = filedialog.askopenfilename(initialdir=USERDIR)
+    l = logging.INFO
+logging.basicConfig(format='%(name)-4s: %(levelname)-8s [%(asctime)s]: %(message)s', level=l)
 
-    file_path = filedialog.askopenfilename(initialdir=USERDIR)
+soldiers_json_file = 'soldiers.json'
+soldiers_csv_file = 'soldiers.csv'
 
 
 class Soldier:
@@ -44,6 +48,7 @@ class Soldier:
         # TODO: Add physical/psi training status
         # TODO: Add current craft
         # TODO: Add inventory and loadout info
+        # TODO: Add service record info
 
 
 class Stats:
@@ -77,144 +82,76 @@ class Stats:
                 self.strength, self.psistrength, self.psiskill]
 
 
-def read_soldiers(data_):
-    soldiers_ = []
-    print('Reading soldier data...')
-    for base in data_['bases']:
+def read_soldiers(_data):
+    _soldiers = []
+    logging.info('Reading soldier data...')
+    for base in _data['bases']:
+        logging.debug(f'Loading soldiers from {base["name"]}...')
         try:
-            for s in base['soldiers']:
-                soldier_ = Soldier(s['type'], s['id'], s['name'], s['initialStats'], s['currentStats'], s['rank'],
-                                   s['missions'], s['kills'], base['name'])
-                soldiers_.append(soldier_)
-        except KeyError:
+            len(base['soldiers'])
+        except KeyError as e:
+            logging.debug(f'Base {base["name"]} has no soldiers; skipping...')
             pass
-    return soldiers_
+        else:
+            for s in base['soldiers']:
+                try:
+                    logging.debug(f'Reading {s["name"]}\'s data')
+                    _soldier = Soldier(s['type'], s['id'], s['name'], s['initialStats'], s['currentStats'], s['rank'],
+                                       s['missions'], s['kills'], base['name'])
+                    _soldiers.append(_soldier)
+                except KeyError as e:
+                    logging.error(f'Error when loading data for {s["name"]}: {e} is not defined.')
+                pass
+    return _soldiers
 
 
-def make_csv(soldiers_):
+def make_csv(_soldiers):
+    logging.info('Creating CSV data...')
     headers = ['ID', 'Base', 'Type', 'Name', 'Rank', 'Missions', 'Kills', 'TUs', 'Stamina', 'Health', 'Bravery',
                'Reactions', 'Firing', 'Throwing', 'Strength', 'PsiStrength', 'PsiSkill', 'Initial TUs',
                'Initial Stamina', 'Initial Health', 'Initial Bravery', 'Initial Reactions', 'Initial Firing',
                'Initial Throwing', 'Initial Strength', 'Initial PsiStrength', 'Initial PsiSkill']
-    csvlist = [headers]
-    for i in soldiers_:
+    csv_list = [headers]
+    for i in _soldiers:
         keys = {
-            'ID'                 : i.id,
-            'Base'               : i.base,
-            'Type'               : i.type,
-            'Name'               : i.name,
-            'Rank'               : i.rank,
-            'Missions'           : i.missions,
-            'Kills'              : i.kills,
-            'TUs'                : i.currentstats.tu,
-            'Stamina'            : i.currentstats.stamina,
-            'Health'             : i.currentstats.health,
-            'Bravery'            : i.currentstats.bravery,
-            'Reactions'          : i.currentstats.reactions,
-            'Firing'             : i.currentstats.firing,
-            'Throwing'           : i.currentstats.throwing,
-            'Strength'           : i.currentstats.strength,
-            'PsiStrength'        : i.currentstats.psistrength,
-            'PsiSkill'           : i.currentstats.psiskill,
-            'Initial TUs'        : i.initialstats.tu,
-            'Initial Stamina'    : i.initialstats.stamina,
-            'Initial Health'     : i.initialstats.health,
-            'Initial Bravery'    : i.initialstats.bravery,
-            'Initial Reactions'  : i.initialstats.reactions,
-            'Initial Firing'     : i.initialstats.firing,
-            'Initial Throwing'   : i.initialstats.throwing,
-            'Initial Strength'   : i.initialstats.strength,
+            'ID': i.id,
+            'Base': i.base,
+            'Type': i.type,
+            'Name': i.name,
+            'Rank': i.rank,
+            'Missions': i.missions,
+            'Kills': i.kills,
+            'TUs': i.currentstats.tu,
+            'Stamina': i.currentstats.stamina,
+            'Health': i.currentstats.health,
+            'Bravery': i.currentstats.bravery,
+            'Reactions': i.currentstats.reactions,
+            'Firing': i.currentstats.firing,
+            'Throwing': i.currentstats.throwing,
+            'Strength': i.currentstats.strength,
+            'PsiStrength': i.currentstats.psistrength,
+            'PsiSkill': i.currentstats.psiskill,
+            'Initial TUs': i.initialstats.tu,
+            'Initial Stamina': i.initialstats.stamina,
+            'Initial Health': i.initialstats.health,
+            'Initial Bravery': i.initialstats.bravery,
+            'Initial Reactions': i.initialstats.reactions,
+            'Initial Firing': i.initialstats.firing,
+            'Initial Throwing': i.initialstats.throwing,
+            'Initial Strength': i.initialstats.strength,
             'Initial PsiStrength': i.initialstats.psistrength,
-            'Initial PsiSkill'   : i.initialstats.psiskill
+            'Initial PsiSkill': i.initialstats.psiskill
         }
         row = []
         for j in headers:
-            print(f'j = {j}')
-            print(f'keys[j] = {keys[j]}')
+            # logging.debug(f'j = {j}')
+            # logging.debug(f'keys[j] = {keys[j]}')
             row.append(keys[j])
-        csvlist.append(row)
-    return csvlist
+        csv_list.append(row)
+    return csv_list
 
 
-data = ''
-soldiers = []
-soldiercsv = []
-
-print(f'Loading data from "{os.path.basename(file_path)}"...')
-with open(file_path, 'r') as file:
-    for y in yaml.load_all(file, Loader=yaml.FullLoader):
-        try:
-            type(y['difficulty'])
-            data = y
-        except KeyError:
-            pass
-
-if jsondump is True:
-    print('Writing converted json data to "data.json"...')
-    with open('data.json', 'w') as outfile:
-        json.dump(data, outfile)
-
-soldiers = read_soldiers(data)
-soldiercsv = make_csv(soldiers)
-
-print('Writing CSV soldier list to "soldiers.csv"...')
-with open('soldiers.csv', 'w', newline='') as csvfile:
-    wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-    wr.writerows(soldiercsv)
-
-
-# # Original ScrollableFrame class copied from https://blog.tecladocode.com/tkinter-scrollable-frames/
-# class ScrollableFrame(ttk.Frame):
-#     def __init__(self, container, *args, **kwargs):
-#         super().__init__(container, *args, **kwargs)
-#         canvas = tk.Canvas(self)
-#         scrollbary = ttk.Scrollbar(self, orient="vertical", command=canvas.yview)
-#         scrollbarx = ttk.Scrollbar(self, orient="horizontal", command=canvas.xview)
-#         self.scrollable_frame = ttk.Frame(canvas)
-#         self.scrollable_frame.bind(
-#                 "<Configure>",
-#                 lambda e: canvas.configure(
-#                         scrollregion=canvas.bbox("all")
-#                 )
-#         )
-#         canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-#         canvas.configure(yscrollcommand=scrollbary.set)
-#         canvas.configure(xscrollcommand=scrollbarx.set)
-#         scrollbarx.pack(side="bottom", fill="x")
-#         scrollbary.pack(side="right", fill="y")
-#         canvas.pack(side="left", fill="both", expand=True)
-#
-#
-# class ScrollableTable:
-#     def __init__(self, table_data):
-#         frame = ScrollableFrame(root)
-#
-#         for i in range(len(table_data)):
-#             for j in range(len(table_data[0])):
-#                 if i is 0:  # Bold the header row
-#                     self.e = Entry(frame.scrollable_frame, width=20, fg='black', font=('Arial', 12, 'bold'))
-#                 else:
-#                     self.e = Entry(frame.scrollable_frame, width=20, fg='black', font=('Arial', 12))
-#                 self.e.grid(row=i, column=j)
-#                 self.e.insert(END, table_data[i][j])
-#         frame.pack(fill="both", expand=True)
-#
-#
-# def create_gui():
-#     root.title("XCOM Soldier Viewer")
-#     width = 1600
-#     height = 900
-#     screen_width = root.winfo_screenwidth()
-#     screen_height = root.winfo_screenheight()
-#     x = (screen_width / 2) - (width / 2)
-#     y = (screen_height / 2) - (height / 2)
-#     root.geometry("%dx%d+%d+%d" % (width, height, x, y))
-#     root.resizable(0, 0)
-#
-#     t = ScrollableTable(soldiercsv)
-
-
-def create_gui():
+def create_soldier_table(csv_data):
     root.title("XCOM Soldier Viewer")
     width = 1600
     height = 900
@@ -223,17 +160,48 @@ def create_gui():
     x = (screen_width / 2) - (width / 2)
     y = (screen_height / 2) - (height / 2)
     root.geometry("%dx%d+%d+%d" % (width, height, x, y))
-    root.resizable(0, 0)
+    root.resizable(1, 1)
 
     tframe = Frame(root)
     tframe.pack(fill="both", expand=True)
-    table = TableCanvas(tframe, editable=False)
-    table.importCSV('soldiers.csv')
+    table = TableCanvas(tframe, read_only=True)
+    table.maxcellwidth = 800
+    table.importCSV(soldiers_csv_file)
     table.show()
+    table.adjustColumnWidths()
+    table.autoResizeColumns()
+    table.redraw()
     table.update()
 
 
-root = Tk()
+def read_save(file):
+    logging.info(f'Loading data from "{os.path.basename(save_file_path)}"...')
+    with open(file, 'r') as file:
+        for y in yaml.load_all(file, Loader=yaml.FullLoader):
+            try:
+                type(y['difficulty'])
+                _data = y
+            except KeyError:
+                pass
 
-create_gui()
+    _soldiers = read_soldiers(_data)
+    _soldier_csv = make_csv(_soldiers)
+
+    if json_dump is True:
+        logging.info(f'Writing converted json data to "{soldiers_json_file}"...')
+        with open(soldiers_json_file, 'w') as outfile:
+            json.dump(_data, outfile)
+
+    logging.info(f'Writing CSV soldier list to "{soldiers_csv_file}"...')
+    with open(soldiers_csv_file, 'w', newline='') as csv_file:
+        wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        wr.writerows(_soldier_csv)
+    return _soldiers, _soldier_csv
+
+
+root = Tk()
+data = ''
+soldiers, soldier_csv = read_save(save_file_path)
+create_soldier_table(soldier_csv)
+
 root.mainloop()
