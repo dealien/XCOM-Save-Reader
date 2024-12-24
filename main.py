98a9de9597fc24873c01a8f7d2c66c6b4a653407
Vyren <dragonofawsome@gmail.com>
2024-12-23 19:13:05 -0500

    Updated tests
    - Resolves #9

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from tkinter import *
 import reader
 
 parser = argparse.ArgumentParser(description="Process command line arguments.")
-parser.add_argument('file', type=str, nargs='?', default=None, help='the path to the save file (optional)')
+parser.add_argument('file', type=str, nargs='?', default=None, help='the relative path to the save file (optional)')
 parser.add_argument('-d', '--debug', action='store_true', help='debug mode')
 parser.add_argument('-j', '--json-dump', action='store_true', help='save json data to file')
 args = parser.parse_args()
 
 print(args)
+print(type(args))
 print(f"File: {args.file}")
 print(f"Debug Mode: {args.debug}")
 print(f"JSON Dump: {args.json_dump}")
