85c860f7ddc90b39be98f83f992bcffdbb51dad5
Vyren <dragonofawsome@gmail.com>
2026-01-21 06:49:53 -0500

    chore: Configure application-wide basic logging

--- src/main.py
+++ src/main.py
@@ -120,5 +120,12 @@ class App(ctk.CTk):
 
 
 if __name__ == "__main__":
+    # Configure logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
     app = App()
     app.mainloop()
